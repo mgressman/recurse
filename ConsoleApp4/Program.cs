76f64ad019abf53bc83935b57483b45d77448fbd
Michael Gressman <mgressman@brightwavepartners.com>
2018-09-07 22:15:45 -0500

    initial check-in

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        private void GetItems(dynamic item, ref object[] items)
+        {
+            if (!(item is IEnumerable))
+            {
+                Array.Resize(ref items, items.Length + 1);
+                items[items.Length - 1] = item;
+            }
+            else
+            {
+                foreach (var i in item)
+                {
+                    if (!(i is IEnumerable))
+                    {
+                        Array.Resize(ref items, items.Length + 1);
+                        items[items.Length - 1] = i;
+                    }
+                    else
+                    {
+                        this.GetItems(i, ref items);
+                    }
+                }
+            }
+        }
+
+        private void Run()
+        {
+            var data = new object[] { 'a', 'b', 1, new object[] {'x', 'y', new object[] {1, 2, new[] {'e'}}, 'r', 't'}, 3, 4, 'i' };
+
+            var flattened = new object[] { };
+
+            this.GetItems(data, ref flattened);
+
+            Console.WriteLine(string.Join(',', flattened));
+            Console.ReadLine();
+        }
+
+        private static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+        }
+    }
+}
